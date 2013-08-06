25dcc39336cbd63828933a3a5acbd5e6ba3d8539
SOMTD <somtd1104@me.com>
2013-08-06 15:29:40 +0900

    initial commit

---
+++ TurnByTurnDirectionSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TurnByTurnDirectionSample
+//
+//  Created by SOMTD on 2013/08/06.
+//  Copyright (c) 2013年 SOMTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TurnByTurnDirectionSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TurnByTurnDirectionSample
+//
+//  Created by SOMTD on 2013/08/06.
+//  Copyright (c) 2013年 SOMTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
